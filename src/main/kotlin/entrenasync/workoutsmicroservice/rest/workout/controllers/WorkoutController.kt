abcdb795dcd1dd84228b64b23d2d9678b3a70067
JAVIER <javier.hernandez@alumno.iesluisvives.org>
2025-05-26 23:00:31 +0200

    FIX: Cors fix

--- src/main/kotlin/entrenasync/workoutsmicroservice/rest/workout/controllers/WorkoutController.kt
+++ src/main/kotlin/entrenasync/workoutsmicroservice/rest/workout/controllers/WorkoutController.kt
@@ -21,7 +21,6 @@ import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/Workouts")
-@CrossOrigin(origins = ["http://localhost:4200"])
 class WorkoutController @Autowired constructor(private val workoutService : IWorkoutService) {
 
     @GetMapping
